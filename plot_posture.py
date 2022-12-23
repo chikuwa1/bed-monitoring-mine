1e3329ab717b403d95c07d4998ea6b20fc36bffa
chikuwa1 <b027ff@yamaguchi-u.ac.jp>
2022-12-23 14:01:27 +0900

    fixed plot process in plot_posture.py

--- plot_posture.py
+++ plot_posture.py
@@ -1,11 +1,13 @@
+# 姿勢毎に被験者のデータをplot
+# 縦軸，横軸はそれぞれのタグのcombination
+
 import json
 import os
 import shutil
 from itertools import combinations
 
 import matplotlib.pyplot as plt
-
-
+import matplotlib.cm as cm
 
 posture_num = 7 # 姿勢数
 
@@ -33,35 +35,44 @@ for tester, tester_data in standardized_data.items():
     for posture, posture_data in tester_data.items():        
         posture_bed_data[posture][tester] = posture_data
 
+
 # 姿勢ごとの図をplot
 
-colors = [] # color map　利用したいYO
+tester_index = {tester: i for i, tester in enumerate(tester_names)}
+ticks_list = [-3,-2,-1,0,1,2,3] # 縦軸横軸のメモリ指定
 
 # 2つのタグの組み合わせ
 tag_combination = list(combinations(range(len(tag_names)), 2))
 
 for posture, posture_data in posture_bed_data.items():
 
-    print("姿勢"+str(posture)+ ":")
-
     dir_path_posture = f'posture_png/posture{posture}/'
     if os.path.exists(dir_path_posture):
         shutil.rmtree(dir_path_posture)
     os.mkdir(dir_path_posture)
     for tag_pair in tag_combination:
-        print("tagペア：" + str(tag_pair))
         x_tag, y_tag = tag_pair
     
-        fig, ax = plt.subplots()
+        # fig, ax = plt.subplots()
+
+        tester_num = 0
         for tester, tester_data in posture_data.items():
-            print(tester)
-            rssis1 = tester_data[tag_dict[x_tag]]
-            rssis2 = tester_data[tag_dict[y_tag]]
-            plt.scatter(rssis1, rssis2, s=20, c=tester, marker='D', alpha=0.05, label = tester)
-        
-        ax.set_xlabel('Tag {}'.format(x_tag))
-        ax.set_ylabel('Tag {}'.format(y_tag))
-        ax.legend()
+            try:
+                rssis1 = tester_data[tag_dict[x_tag]]
+                rssis2 = tester_data[tag_dict[y_tag]]
+                plt.scatter(rssis1, rssis2, s=20, c=[cm.Paired(tester_num)], marker='o', label = tester)
+                plt.xticks(ticks_list)
+                plt.yticks(ticks_list)
+                tester_num += 1
+            except:
+                print("姿勢：" + str(posture) + ", tag_pair:" + str(tag_pair) + ", " + str(tester))
+
+        plt.xlabel('Tag {}'.format(x_tag))
+        plt.ylabel('Tag {}'.format(y_tag))
+        plt.legend()
+        # ax.set_xlabel('Tag {}'.format(x_tag))
+        # ax.set_ylabel('Tag {}'.format(y_tag))
+        # ax.legend()
         file_name = 'tag{}_tag{}.png'.format(x_tag, y_tag)
         plt.savefig(os.path.join(dir_path_posture, file_name))
         plt.clf() # ないとメモリが完全に解放されない
--- test_pro.py
+++ test_pro.py
@@ -1,104 +1,124 @@
-# import pandas as pd
-# from datetime import datetime
 import json
+from sklearn import preprocessing
+import numpy as np
 
-posture_num = 7 # 姿勢数
+list_m = [1,2,3,4,5,6,7,8,9,10]
 
-with open("bed_data.json", "r") as json_file:
-    bed_data = json.load(json_file)
+np_list = np.array(list_m).reshape(-1, 1)
+# StandardScaler クラスを使用して、データを標準化する
+sc = preprocessing.StandardScaler()
+sc.fit(np_list)
+standardized_list = sc.transform(np_list)
+list_v = list(standardized_list.reshape(-1))
 
-# print(bed_data["furushima"]["1"]["E280116060000204AC6AD1FE"][0])    
+print(list_v)
 
-with open('tester_name.txt') as f: # 被験者名の読み込み
-    tester_names = f.read().splitlines()
+
+
+
+
+
+# # import pandas as pd
+# # from datetime import datetime
+# import json
+
+# posture_num = 7 # 姿勢数
+
+# with open("bed_data.json", "r") as json_file:
+#     bed_data = json.load(json_file)
+
+# # print(bed_data["furushima"]["1"]["E280116060000204AC6AD1FE"][0])    
+
+# with open('tester_name.txt') as f: # 被験者名の読み込み
+#     tester_names = f.read().splitlines()
  
-with open('tag_name.txt') as f: # タグ名の読み込み
-    tag_names = f.read().splitlines()
+# with open('tag_name.txt') as f: # タグ名の読み込み
+#     tag_names = f.read().splitlines()
 
 
-# 各タグ，各時刻のデータ数とrssi平均値の取得
+# # 各タグ，各時刻のデータ数とrssi平均値の取得
 
-tag_count_data = {}  # 各タグ，各時刻のデータ取得数
-avg_bed_data = {}  # 各タグ，各時刻の平均値
+# tag_count_data = {}  # 各タグ，各時刻のデータ取得数
+# avg_bed_data = {}  # 各タグ，各時刻の平均値
 
 
-# for tester in tester_names:
-tester = "furushima"
-tag_count_data[tester] = {}
-avg_bed_data[tester] = {}
-# for i in range(posture_num):
-i = 1 # 姿勢1
+# # for tester in tester_names:
+# tester = "furushima"
+# tag_count_data[tester] = {}
+# avg_bed_data[tester] = {}
+# # for i in range(posture_num):
+# i = 1 # 姿勢1
     
-count = 0  # 毎秒のデータ数カウンター
-sum = 0.0  # 毎秒のrssi値の合計値
-avg_data = {}
-count_one_tag = {}
-max_second = 0  # maxの秒数
-for tag in tag_names:
-    if tag in bed_data[tester][str(i)] and bed_data[tester][str(i)][tag][-1][0]:
-        if max_second < bed_data[tester][str(i)][tag][-1][0]:
-            max_second = bed_data[tester][str(i)][tag][-1][0]
-        else:
-            break
-    # print(tester, i, tag, max_second)
+# count = 0  # 毎秒のデータ数カウンター
+# sum = 0.0  # 毎秒のrssi値の合計値
+# avg_data = {}
+# count_one_tag = {}
+# max_second = 0  # maxの秒数
 # for tag in tag_names:
-tag = "E280116060000204AC6AD1FD"
-if tag not in avg_data:
-    avg_data[tag] = []
-if tag not in count_one_tag:
-    count_one_tag[tag] = []
+#     if tag in bed_data[tester][str(i)] and bed_data[tester][str(i)][tag][-1][0]:
+#         if max_second < bed_data[tester][str(i)][tag][-1][0]:
+#             max_second = bed_data[tester][str(i)][tag][-1][0]
+#         else:
+#             break
+#     # print(tester, i, tag, max_second)
+# # for tag in tag_names:
+# tag = "E280116060000204AC6AD1FD"
+# if tag not in avg_data:
+#     avg_data[tag] = []
+# if tag not in count_one_tag:
+#     count_one_tag[tag] = []
 
-if tag in bed_data[tester][str(i)]:
-    # print(tester + "の姿勢" + str(i) + "のタグ" + tag + "はデータが存在します")
-    second = 0  # 秒数
-    idx = 0  # データのindex
-    while second <= max_second:
-        if idx < len(bed_data[tester][str(i)][tag]) :
+# if tag in bed_data[tester][str(i)]:
+#     # print(tester + "の姿勢" + str(i) + "のタグ" + tag + "はデータが存在します")
+#     second = 0  # 秒数
+#     idx = 0  # データのindex
+#     while second <= max_second:
+#         if idx < len(bed_data[tester][str(i)][tag]) :
             
-            if bed_data[tester][str(i)][tag][idx][0] == second:
-                print(str(second) + "sec 合計：" + str(sum) + " 加える値：" + str(bed_data[tester][str(i)][tag][idx][1]))
-                count += 1
-                sum += bed_data[tester][str(i)][tag][idx][1]
-                idx += 1
-            else: 
-                count_one_tag[tag].append(count)
-                second += 1
-                if count == 0:
-                    print(str(second - 1) + "秒のデータなし！泣")   
-                    avg_data[tag].append(-110.0)
-                    print("平均値：（最低）"+ str(avg_data[tag][second-1]))
-                else:
-                    avg_data[tag].append(sum/count)
-                    print(str(second - 1) + "秒の平均は")
-                    print("合計：" + str(sum) + " 数：" +  str(count))
-                    print("平均値：" + str(avg_data[tag][second-1]))
-                    count = 0
-                    sum = 0.0
+#             if bed_data[tester][str(i)][tag][idx][0] == second:
+#                 print(str(second) + "sec 合計：" + str(sum) + " 加える値：" + str(bed_data[tester][str(i)][tag][idx][1]))
+#                 count += 1
+#                 sum += bed_data[tester][str(i)][tag][idx][1]
+#                 idx += 1
+#             else: 
+#                 count_one_tag[tag].append(count)
+#                 second += 1
+#                 if count == 0:
+#                     print(str(second - 1) + "秒のデータなし！泣")   
+#                     avg_data[tag].append(-110.0)
+#                     print("平均値：（最低）"+ str(avg_data[tag][second-1]))
+#                 else:
+#                     avg_data[tag].append(sum/count)
+#                     print(str(second - 1) + "秒の平均は")
+#                     print("合計：" + str(sum) + " 数：" +  str(count))
+#                     print("平均値：" + str(avg_data[tag][second-1]))
+#                     count = 0
+#                     sum = 0.0
                 
-        else:
-            count_one_tag[tag].append(count)
-            second += 1
-            if count == 0:
-                avg_data[tag].append(-110.0)
-            else:
-                avg_data[tag].append(sum/count)
-                print(str(second - 1) + "秒の平均は")
-                print("合計：" + str(sum) + " 数：" +  str(count))
-                print("平均値：" + str(avg_data[tag][second-1]))
-                count = 0
-                sum = 0.0
+#         else:
+#             count_one_tag[tag].append(count)
+#             second += 1
+#             if count == 0:
+#                 avg_data[tag].append(-110.0)
+#             else:
+#                 avg_data[tag].append(sum/count)
+#                 print(str(second - 1) + "秒の平均は")
+#                 print("合計：" + str(sum) + " 数：" +  str(count))
+#                 print("平均値：" + str(avg_data[tag][second-1]))
+#                 count = 0
+#                 sum = 0.0
 
-    if count != 0:
-        count_one_tag[tag].append(count)
-        avg_data[tag].append(sum/count)
-        print(str(second - 1) + "秒の平均は")
-        print("合計：" + str(sum) + " 数：" +  str(count))
-        print("平均値：" + str(avg_data[tag][second-1]))
-        count = 0
-        sum = 0.0
+#     if count != 0:
+#         count_one_tag[tag].append(count)
+#         avg_data[tag].append(sum/count)
+#         print(str(second - 1) + "秒の平均は")
+#         print("合計：" + str(sum) + " 数：" +  str(count))
+#         print("平均値：" + str(avg_data[tag][second-1]))
+#         count = 0
+#         sum = 0.0
 
-# else: print(tester + "の姿勢" + str(i) + "のタグ" + tag + "はデータが存在しません")
-tag_count_data[tester][str(i)] = count_one_tag
-avg_bed_data[tester][str(i)] = avg_data
+# # else: print(tester + "の姿勢" + str(i) + "のタグ" + tag + "はデータが存在しません")
+# tag_count_data[tester][str(i)] = count_one_tag
+# avg_bed_data[tester][str(i)] = avg_data
 
-# print(bed_data["furushima"]["1"]["E280116060000204AC6AD1FD"])
+# # print(bed_data["furushima"]["1"]["E280116060000204AC6AD1FD"])
