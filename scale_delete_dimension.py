e8d137a17a69a9d260bcd866fd81b217d0cbd6a7
chikuwa1 <b027ff@yamaguchi-u.ac.jp>
2023-01-27 12:57:28 +0900

    added standardized PCA

---
+++ scale_delete_dimension.py
@@ -0,0 +1,111 @@
+import os
+import shutil
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+
+posture_num = 7 # 姿勢数
+tester_num = 11 # 被験者数
+
+# ファイルの確認
+# PCA
+dir_path_PCA = 'posture_png/pca_png/sc_pca_png'
+
+if os.path.exists(dir_path_PCA):
+    shutil.rmtree(dir_path_PCA)
+os.mkdir(dir_path_PCA)
+
+p_file = open('sc_pca_contribution_rate.txt', 'w')
+
+
+# SVD
+# dir_path_SVD = 'posture_png/svd_png/'
+# if os.path.exists(dir_path_SVD):
+#     shutil.rmtree(dir_path_SVD)
+# os.mkdir(dir_path_SVD)
+
+# s_file = open('svd_contribution_rate.txt', 'w')
+
+df = pd.read_csv("posture_bed_data.csv") # 値や表の形は大丈夫
+df = pd.get_dummies(df, drop_first=True)
+
+for posture in range(posture_num):
+    df_posture = df[df['posture'] == posture]
+    df_rssis = df_posture.drop(['posture', 'tester'], axis=1) # 各姿勢の全員分のrssiデータをタグごとにまとめたもの（各姿勢，各被験者名の情報なし）
+    # 標準化
+    sc = StandardScaler()
+    df_std = sc.fit_transform(df_rssis)
+
+    # PCA
+    pca = PCA(n_components=2)
+    df_pca = pd.DataFrame(pca.fit_transform(df_std)) # 第一主成分0と第二主成分1がわかる
+    
+    df_pca['tester'] = df[df['posture'] == posture]['tester'].reset_index(drop=True)
+     #うまくできてそう->姿勢6で検証:df_pca各被験者のデータ数はlabel追加前のデータ数と一致を確認
+    
+    for tester in range(tester_num):
+        x_pca = df_pca[df_pca['tester'] == tester][0]
+        y_pca = df_pca[df_pca['tester'] == tester][1]
+        plt.scatter(x_pca, y_pca, s=20, c=[cm.Paired(tester)], marker='o', label = 'tester' + str(tester))
+    
+    plt.xlabel('0')
+    plt.ylabel('1')
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1.05))
+    plt.tight_layout()
+    # plt.title('posture{}_PCA'.format(posture))
+    file_name = 'posture{}_sc_PCA.png'.format(posture)
+    plt.savefig(os.path.join(dir_path_PCA, file_name))
+    plt.clf()
+    plt.close()
+
+    # 寄与率
+    explained_variance_ratio_df = pd.DataFrame(pca.explained_variance_ratio_)
+    p_file.write("========== posture"+ str(posture) + " ==========\n")
+    p_file.write("<第一主成分0，第二主成分1の寄与率>\n")
+    p_file.write(str(explained_variance_ratio_df)+ "\n")
+    p_file.write('\n')
+
+    # 各タグの寄与率
+    loadings_df = pd.DataFrame(pca.components_.T, index=df_rssis.columns)
+    p_file.write("<第一主成分0，第二主成分1に対する各タグの固有ベクトル>\n")
+    p_file.write(str(loadings_df)+ "\n")
+    p_file.write('\n')
+
+    # # SVD
+    # svd = TruncatedSVD(n_components=2)
+    # df_svd = pd.DataFrame(svd.fit_transform(df_rssis))
+    # df_svd['tester'] = df[df['posture'] == posture]['tester'].reset_index(drop=True)
+    
+    # for tester in range(tester_num):
+    #     x_svd = df_svd[df_svd['tester'] == tester][0]
+    #     y_svd = df_svd[df_svd['tester'] == tester][1]
+    #     plt.scatter(x_svd, y_svd, s=20, c=[cm.Paired(tester)], marker='o', label = 'tester' + str(tester))
+    
+    # plt.xlabel('0')
+    # plt.ylabel('1')
+    # plt.legend(loc='upper left', bbox_to_anchor=(1, 1.05))
+    # plt.tight_layout()
+    # # plt.title('posture{}_SVD'.format(posture))
+    # file_name = 'posture{}_SVD.png'.format(posture)
+    # plt.savefig(os.path.join(dir_path_SVD, file_name))
+    # plt.clf()
+    # plt.close()
+
+    # # 寄与率
+    # explained_variance_ratio_df = pd.DataFrame(svd.explained_variance_ratio_)
+    # s_file.write("========== posture"+ str(posture) + " ==========\n")
+    # s_file.write("<第一主成分0，第二主成分1の寄与率>\n")
+    # s_file.write(str(explained_variance_ratio_df)+ "\n")
+    # s_file.write('\n')
+
+    # # 各タグの寄与率
+    # loadings_df = pd.DataFrame(svd.components_.T, index=df_rssis.columns)
+    # s_file.write("<第一主成分0，第二主成分1に対する各タグの固有ベクトル>\n")
+    # s_file.write(str(loadings_df)+ "\n")
+    # s_file.write('\n')
+
+p_file.close()
+# s_file.close()
--- test.py
+++ test.py
@@ -1,33 +1,30 @@
-# 数値計算やデータフレーム操作に関するライブラリをインポートする
 import numpy as np
 import pandas as pd
-# URL によるリソースへのアクセスを提供するライブラリをインポートする。
-import urllib.request 
-# 図やグラフを図示するためのライブラリをインポートする。
 import matplotlib.pyplot as plt
-# %matplotlib inline
 import sklearn #機械学習のライブラリ
 from sklearn.decomposition import PCA #主成分分析器
 
-url = "https://raw.githubusercontent.com/maskot1977/ipython_notebook/master/toydata/wine.txt"
-urllib.request.urlretrieve(url, 'wine.txt') 
+with open('tag_name.txt', 'r') as f: # タグ名の読み込み
+    tag_names = f.read().splitlines()
 
-# データの書き込み
-df = pd.read_csv("wine.txt", sep="\t", index_col=0)
 
-print(df)
+df = pd.read_csv("posture_bed_data.csv")
+df_p = df[df['posture'] == 1]
+# print(df_p)
+dfs = df_p.iloc[:, 2:].apply(lambda x: (x-x.mean())/x.std(), axis=0)
+# print(dfs)
+for tag in tag_names:
+    if dfs[tag].isnull().all(): 
+        dfs_t = dfs.drop(tag, axis=1)
+# print(str(dfs_t))
 
-# # 標準化（一列目はclassなので，一列目以外をいじり，第一列目で色分けすることでデータを観測）
-# dfs = df.iloc[:, 1:].apply(lambda x: (x-x.mean())/x.std(), axis=0)
+for tag in tag_names:
+    if dfs[tag].isnull().all(): 
+        dfs_t = dfs.drop(tag, axis=1)
 
-# # 主成分分析の実行
+print(str(dfs_t))
+# #主成分分析の実行
 # pca = PCA()
 # pca.fit(dfs)
-
 # # データを主成分空間に写像
 # feature = pca.transform(dfs)
-
-# # 主成分得点
-# print(str(pd.DataFrame(feature, columns=["PC{}".format(x + 1) for x in range(len(dfs.columns))])))
-
-
