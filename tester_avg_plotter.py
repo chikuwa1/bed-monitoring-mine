5d8744bf512e3a61365fc3f7804a17d9a868c427
chikuwa1 <b027ff@yamaguchi-u.ac.jp>
2023-03-30 18:40:18 +0900

    fixed tester_avg_plotter.py

--- tester_avg_plotter.py
+++ tester_avg_plotter.py
@@ -1,4 +1,4 @@
-# 姿勢毎に被験者の平均化したデータをplot
+# 姿勢毎に被験者の平均化したデータをplotする
 # 縦軸，横軸はそれぞれのタグのcombination
 
 import json
@@ -11,6 +11,8 @@ import matplotlib.cm as cm
 
 posture_num = 7 # 姿勢数
 
+
+# ファイルの読み込み
 with open("avg_bed_data.json", "r") as json_file:
     average_data = json.load(json_file)
 
@@ -20,7 +22,9 @@ with open('tester_name.txt') as f: # 被験者名の読み込み
 with open('tag_name.txt', 'r') as f: # タグ名の読み込み
     tag_names = f.readlines()
 
-tag_dict = {} # tagの名前を0から順に対応させる
+
+# tag名を0から順に対応させる
+tag_dict = {}
 for i, tag_name in enumerate(tag_names):
     tag_dict[i] = tag_name.strip()
 
@@ -28,20 +32,19 @@ tester_bed_data = average_data
 
 # 姿勢ごとの図をplot
 
-# tester_index = {tester: i for i, tester in enumerate(tester_names)}
-
 # 2つのタグの組み合わせ
 tag_combination = list(combinations(range(len(tag_names)), 2))
 
 for tester, tester_data in tester_bed_data.items():
-    dir_path_tester = f'tester_png/average_ln_png/l_tester_{tester}/'
+    dir_path_tester = f'tester_png/average_png/tester_{tester}/'
+
+    # ディレクトリが存在していたら削除後、再度作成
     if os.path.exists(dir_path_tester):
         shutil.rmtree(dir_path_tester)
     os.mkdir(dir_path_tester)
+
     for tag_pair in tag_combination:
         x_tag, y_tag = tag_pair
-    
-        # fig, ax = plt.subplots()
 
         for posture, posture_data in tester_data.items():
             try:
@@ -52,11 +55,11 @@ for tester, tester_data in tester_bed_data.items():
                 plt.ylim(-80.0, -55.0)
                 
             except:
-                print("tester:" + str(tester) + ", tag_pair:" + str(tag_pair) + ", " + str(posture))
+                print("エラー箇所 > tester:" + str(tester) + ", tag_pair:" + str(tag_pair) + ", " + str(posture))
 
         plt.xlabel('Tag {}'.format(x_tag))
         plt.ylabel('Tag {}'.format(y_tag))
-        # plt.legend(loc='upper left', bbox_to_anchor=(1, 1.05))
+        plt.legend(loc='upper left', bbox_to_anchor=(1, 1.05))
         plt.tight_layout()
         file_name = 'tag{}_tag{}.png'.format(x_tag, y_tag)
         plt.savefig(os.path.join(dir_path_tester, file_name))
